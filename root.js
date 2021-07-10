2e6f1ca2025c9ea5acd02aafe73096485dc38626
akki4134 <yellagandulaakhil@gmail.com>
2021-07-10 15:37:26 +0530

    setup

---
+++ root.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+//import routes
+//const authRoute = require('./Routes/userRoute')
+
+dotenv.config();
+
+//connect Database
+mongoose.connect(process.env.DB_CONNECTION, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected')
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+
+
+
+//Middleware
+//app.use(express.json()) // to send post request
+
+//Route middleware
+//app.use('/api/user', authRoute)
+
+
+app.listen(8080, () => console.log("server connected"))
